1be7b85f29d5b1d42fe3ea4d4d3e79027cd3a9ec
JpGamer000 <163525423+JpGamer000@users.noreply.github.com>
2024-08-28 22:49:43 -0300

    Script.js

--- Script.js
+++ Script.js
@@ -2,10 +2,46 @@
 function exercicio1() {
     let nota = prompt("Digite uma nota entre 0 e 10:");
         
-    while (nota < 0 || nota > 10);
+    while (nota < 0 || nota > 10){
     
-    prompt("Digite uma nota de 0 a 10);
+    }prompt("Digite uma nota de 0 a 10);
     if ( nota >= 0 || nota <= 10) {
     
     alert("Sua nota é:" + nota);
 }
+
+    
+ function exercicio2() {
+     let Nome = prompt("Digite seu nome de usuário");
+     let senha = prompt("Digite uma senhas");
+     
+      while( Nome == senha){
+     
+     Nome = prompt("Digite seu nome de usuário");
+     senha = prompt("Digite uma senha");
+    }alert("Senha segura");
+ }
+
+    
+function exercicio3() {
+    let Div = parseInt(prompt("Digite quantos números você deseja dividir"));
+    let Soma = 0;
+    let x = 0;
+
+     while(x < Div){
+         let num = parseInt(prompt("Digite um numero"));
+         Soma += num;
+         x++;
+
+         let media = div/soma;
+     }alert("media é =" + media);
+}
+        
+         
+    
+    
+    
+     
+
+     
+     
